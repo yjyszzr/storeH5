8e099ab60b11468f4d351dab3d230c565f945a79
wenhuaiteng <wenhuaiteng@126.com>
2019-01-13 16:46:21 +0800

    增加乐透代理接口...

--- storeH5-service/src/main/java/com/dl/store/web/LottoController.java
+++ storeH5-service/src/main/java/com/dl/store/web/LottoController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.dl.base.param.EmptyParam;
 import com.dl.base.result.BaseResult;
+import com.dl.lottery.dto.OrderIdDTO;
 import com.dl.lotto.api.ISuperLottoService;
 import com.dl.lotto.dto.LottoFirstDTO;
+import com.dl.lotto.param.SaveBetInfoParam;
+import com.dl.order.api.IOrderService;
+import com.dl.order.dto.LottoOrderDetailDTO;
+import com.dl.order.param.OrderDetailParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,15 +26,29 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/lotto")
 @Slf4j
 public class LottoController {
-
 	@Resource
 	private ISuperLottoService iLottoService;
+	@Resource
+	private IOrderService iOrderService;
 	
 	@ApiOperation(value = "选号投注页数据", notes = "选号投注页数据")	
 	@PostMapping("/getTicketInfo")
     public BaseResult<LottoFirstDTO> getTiketInfo(@RequestBody EmptyParam param){
+		log.info("[getTiketInfo]");
 		return iLottoService.getTicketInfo(param);
 	}
-
-
+	
+	@ApiOperation(value = "生成模拟订单", notes = "生成模拟订单")	
+	@PostMapping("/createOrderSimulate")
+    public BaseResult<OrderIdDTO> createOrderSimulateByStore(@RequestBody SaveBetInfoParam param){
+		log.info("[createOrderSimulateByStore]");
+		return iLottoService.createOrderSimulateByStore(param);
+	}
+	
+	@ApiOperation(value = "获取乐透详情", notes = "获取乐透详情")
+	@PostMapping("/getLottoDetail")
+	public BaseResult<LottoOrderDetailDTO> getLottoOrderDetailByStore(@RequestBody OrderDetailParam param){
+		log.info("[getLottoOrderDetailByStore]");
+		return iOrderService.getLottoOrderDetailSimulatByStore(param);
+	}
 }
