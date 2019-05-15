7585c8ceedcfd73a5ed4a146ce8bfc04303e83c6
zhangzirong <zhangzirong@caixiaomi.com>
2019-05-15 18:51:30 +0800

    Update Order.java

--- storeH5-service/src/main/java/com/dl/store/model/Order.java
+++ storeH5-service/src/main/java/com/dl/store/model/Order.java
@@ -268,4 +268,348 @@ public class Order {
     public void setOrderId(Integer orderId) {
         this.orderId = orderId;
     }
+
+    public String getOrderSn() {
+        return orderSn;
+    }
+
+    public void setOrderSn(String orderSn) {
+        this.orderSn = orderSn;
+    }
+
+    public String getParentSn() {
+        return parentSn;
+    }
+
+    public void setParentSn(String parentSn) {
+        this.parentSn = parentSn;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(Integer orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Integer getPrintLotteryStatus() {
+        return printLotteryStatus;
+    }
+
+    public void setPrintLotteryStatus(Integer printLotteryStatus) {
+        this.printLotteryStatus = printLotteryStatus;
+    }
+
+    public BigDecimal getPrintLotteryRefundAmount() {
+        return printLotteryRefundAmount;
+    }
+
+    public void setPrintLotteryRefundAmount(BigDecimal printLotteryRefundAmount) {
+        this.printLotteryRefundAmount = printLotteryRefundAmount;
+    }
+
+    public Integer getPayStatus() {
+        return payStatus;
+    }
+
+    public void setPayStatus(Integer payStatus) {
+        this.payStatus = payStatus;
+    }
+
+    public Integer getPayId() {
+        return payId;
+    }
+
+    public void setPayId(Integer payId) {
+        this.payId = payId;
+    }
+
+    public String getPayCode() {
+        return payCode;
+    }
+
+    public void setPayCode(String payCode) {
+        this.payCode = payCode;
+    }
+
+    public String getPayName() {
+        return payName;
+    }
+
+    public void setPayName(String payName) {
+        this.payName = payName;
+    }
+
+    public String getPaySn() {
+        return paySn;
+    }
+
+    public void setPaySn(String paySn) {
+        this.paySn = paySn;
+    }
+
+    public BigDecimal getMoneyPaid() {
+        return moneyPaid;
+    }
+
+    public void setMoneyPaid(BigDecimal moneyPaid) {
+        this.moneyPaid = moneyPaid;
+    }
+
+    public BigDecimal getTicketAmount() {
+        return ticketAmount;
+    }
+
+    public void setTicketAmount(BigDecimal ticketAmount) {
+        this.ticketAmount = ticketAmount;
+    }
+
+    public BigDecimal getSurplus() {
+        return surplus;
+    }
+
+    public void setSurplus(BigDecimal surplus) {
+        this.surplus = surplus;
+    }
+
+    public BigDecimal getUserSurplus() {
+        return userSurplus;
+    }
+
+    public void setUserSurplus(BigDecimal userSurplus) {
+        this.userSurplus = userSurplus;
+    }
+
+    public BigDecimal getUserSurplusLimit() {
+        return userSurplusLimit;
+    }
+
+    public void setUserSurplusLimit(BigDecimal userSurplusLimit) {
+        this.userSurplusLimit = userSurplusLimit;
+    }
+
+    public BigDecimal getThirdPartyPaid() {
+        return thirdPartyPaid;
+    }
+
+    public void setThirdPartyPaid(BigDecimal thirdPartyPaid) {
+        this.thirdPartyPaid = thirdPartyPaid;
+    }
+
+    public Integer getUserBonusId() {
+        return userBonusId;
+    }
+
+    public void setUserBonusId(Integer userBonusId) {
+        this.userBonusId = userBonusId;
+    }
+
+    public BigDecimal getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(BigDecimal bonus) {
+        this.bonus = bonus;
+    }
+
+    public Integer getGiveIntegral() {
+        return giveIntegral;
+    }
+
+    public void setGiveIntegral(Integer giveIntegral) {
+        this.giveIntegral = giveIntegral;
+    }
+
+    public String getOrderFrom() {
+        return orderFrom;
+    }
+
+    public void setOrderFrom(String orderFrom) {
+        this.orderFrom = orderFrom;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public Integer getAddTime() {
+        return addTime;
+    }
+
+    public void setAddTime(Integer addTime) {
+        this.addTime = addTime;
+    }
+
+    public Integer getPayTime() {
+        return payTime;
+    }
+
+    public void setPayTime(Integer payTime) {
+        this.payTime = payTime;
+    }
+
+    public Integer getOrderType() {
+        return orderType;
+    }
+
+    public void setOrderType(Integer orderType) {
+        this.orderType = orderType;
+    }
+
+    public Integer getLotteryClassifyId() {
+        return lotteryClassifyId;
+    }
+
+    public void setLotteryClassifyId(Integer lotteryClassifyId) {
+        this.lotteryClassifyId = lotteryClassifyId;
+    }
+
+    public String getDeviceChannel() {
+        return deviceChannel;
+    }
+
+    public void setDeviceChannel(String deviceChannel) {
+        this.deviceChannel = deviceChannel;
+    }
+
+    public Integer getLotteryPlayClassifyId() {
+        return lotteryPlayClassifyId;
+    }
+
+    public void setLotteryPlayClassifyId(Integer lotteryPlayClassifyId) {
+        this.lotteryPlayClassifyId = lotteryPlayClassifyId;
+    }
+
+    public Integer getAwardTime() {
+        return awardTime;
+    }
+
+    public void setAwardTime(Integer awardTime) {
+        this.awardTime = awardTime;
+    }
+
+    public Integer getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(Integer storeId) {
+        this.storeId = storeId;
+    }
+
+    public Date getMatchTime() {
+        return matchTime;
+    }
+
+    public void setMatchTime(Date matchTime) {
+        this.matchTime = matchTime;
+    }
+
+    public BigDecimal getWinningMoney() {
+        return winningMoney;
+    }
+
+    public void setWinningMoney(BigDecimal winningMoney) {
+        this.winningMoney = winningMoney;
+    }
+
+    public String getPassType() {
+        return passType;
+    }
+
+    public void setPassType(String passType) {
+        this.passType = passType;
+    }
+
+    public String getPlayType() {
+        return playType;
+    }
+
+    public void setPlayType(String playType) {
+        this.playType = playType;
+    }
+
+    public Integer getCathectic() {
+        return cathectic;
+    }
+
+    public void setCathectic(Integer cathectic) {
+        this.cathectic = cathectic;
+    }
+
+    public Integer getBetNum() {
+        return betNum;
+    }
+
+    public void setBetNum(Integer betNum) {
+        this.betNum = betNum;
+    }
+
+    public Integer getAcceptTime() {
+        return acceptTime;
+    }
+
+    public void setAcceptTime(Integer acceptTime) {
+        this.acceptTime = acceptTime;
+    }
+
+    public Integer getTicketTime() {
+        return ticketTime;
+    }
+
+    public void setTicketTime(Integer ticketTime) {
+        this.ticketTime = ticketTime;
+    }
+
+    public String getForecastMoney() {
+        return forecastMoney;
+    }
+
+    public void setForecastMoney(String forecastMoney) {
+        this.forecastMoney = forecastMoney;
+    }
+
+    public String getIssue() {
+        return issue;
+    }
+
+    public void setIssue(String issue) {
+        this.issue = issue;
+    }
+
+    public Integer getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(Integer isDelete) {
+        this.isDelete = isDelete;
+    }
+
+    public Integer getTicketNum() {
+        return ticketNum;
+    }
+
+    public void setTicketNum(Integer ticketNum) {
+        this.ticketNum = ticketNum;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
 }
